20fabe56abf350eba823526f01da91f45cf010aa
Yingying <92928939+yingyinghao@users.noreply.github.com>
2024-06-26 11:15:57 -0400

    nav bar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // Enables the new app directory structure
+    appDir: true,
+
+    // Allows server components to use external packages
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+
+  images: {
+    // Specifies the domains from which images can be loaded
+    domains: ["lh3.googleusercontent.com"],
+  },
+
+  webpack(config) {
+    // Enables the top-level await feature in Webpack
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
